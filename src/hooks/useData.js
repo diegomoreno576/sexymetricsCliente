66283bec7de2faa8c7e6ac71bb998d97448288c3
diego576 <diegomoreno576@gmail.com>
2022-12-03 17:02:01 +0100

    first commit

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -8,11 +8,10 @@ const useData = (API_URL, START, END) => {
   const [state, dispatch] = useContext(ThemeContext);
   const [Data, setData] = useState([]);
   const [blog_id, setblog_id] = useState(JSON.parse(window.sessionStorage.getItem('blog')))
-  console.log("blog id",blog_id.code)
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_BASEURL}${API_URL}?${process.env.REACT_APP_USERTOKEN}&blogId=${blog_id.code}&start=${START}&end=${END}&${process.env.REACT_APP_USERID}&${process.env.REACT_APP_USUARIO}`)
+    fetch(`${process.env.REACT_APP_BASEURL}${API_URL}?${process.env.REACT_APP_USERTOKEN}&blogId=33903&start=${START}&end=${END}&${process.env.REACT_APP_USERID}&${process.env.REACT_APP_USUARIO}`)
       .then((response) => response.json())
-      .then((data) => setData(data))
+      .then((data) => setData(data, console.log(data)))
       .catch((error) => console.log(error.message));
       
       dispatch(setLoading(false));
